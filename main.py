650c9028fbf45f630db04b9e4786ed42f6eb1a38
Matthias <eurocoinsalbum@users.noreply.github.com>
2021-10-08 13:35:23 +0200

    Added code for step B)

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import random
+import time
 
-value = random.randint(1, 2)
-if (value == 1):
-    print("black")
-elif (value == 2):
-    print("red")
+for x in range(10):
+    value = random.randint(1, 2)
+    if (value == 1):
+        print("black")
+    elif (value == 2):
+        print("red")
+    
+    time.sleep(1.)
