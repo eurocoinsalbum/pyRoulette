d8ee998133321a67c6433f71a8224c2b0ea5be81
Matthias <eurocoinsalbum@users.noreply.github.com>
2021-10-08 13:37:37 +0200

    Added code for step C)

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import random
 import time
 
+money = 100
+
 for x in range(10):
     value = random.randint(1, 2)
     if (value == 1):
         print("black")
+        money -= 100
     elif (value == 2):
         print("red")
-    
+        money += 100
+
+    print("Amount: " + str(money))    
     time.sleep(1.)
