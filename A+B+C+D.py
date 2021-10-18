0a5925f8eff7314f9818a7205700f5ee5896ef06
micutadiana <92055127+micutadiana@users.noreply.github.com>
2021-10-18 23:48:02 +0200

    Update A+B+C+D.py

--- A+B+C+D.py
+++ A+B+C+D.py
@@ -5,15 +5,21 @@ money = []
 
 for x in range(10):
     money.append(float(input("How much do you want to bet?\n")))
-    user_choice = input('Do you choose 1-black or 2-red?\n')
 
-    random.choice = random.randint(1, 2)
-    if (choice == 1):
+    user_choice = input('Do you choose 1-black or 2-red?\n')
+    if (user_choice == 1):
         print("black")
         money -= []
-    elif (choice == 2):
+    elif (user_choice == 2):
         print("red")
-        money += [6]
+        money += []
 
+    value = random.randint(1, 2)
+    if (value == user_choice):
+        money -= 100
+    elif (value == 2):
+        print("red")
+        money += 100
+   
     print("Amount: " + str(money))    
     time.sleep(1.)
